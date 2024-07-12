89d74990ffbaf2dd7c95f977c3522391e460b23c
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-12 18:40:23 +0530

    Start with Laravel,understand folder structure and how basic routing works

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo',function(){
+    echo "Hello Laravel, Laravel is so cool";
+});
+
+// Route::get('/new/',function(){
+//     return view('demo');
+// });
+// Route::get('/new/{name}',function($name){
+//     echo $name;
+//     return view('demo');
+// });
+// Route::get('/new/{name}/{id?}',function($name,$id = null){
+//     echo $name;
+//     // echo $id;
+//     return view('demo');
+// });
+Route::get('/new/{name}/{id?}',function($name,$id = null){
+    $data = compact("name","id");
+    // print_r($data);
+    // echo $name;
+    return view('demo')->with($data); // with() is used to give data of url to blade.php file
+});
+// Route::post('/test',function(){
+//     echo "Testing the data";
+// });
